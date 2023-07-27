e1571c416bba5b3ce08400a7f4e6eeeeb8d90e56
Rafal <111435736+RafalRudnik@users.noreply.github.com>
2023-07-27 14:10:35 +0200

    menu

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,13 @@ const mobbDiv = document.querySelector('.mobbdeep');
 const nasAudio = new Audio('../Nas.mp3');
 const nasDiv = document.querySelector('.artists__images-nas');
 const currentYear = document.querySelector('footer p span');
-const menuBtn = document.querySelector('.nav__burger')
+const menuBtn = document.querySelector('.nav__burger');
+const nav = document.querySelector('.nav__list');
+const allLinks = document.querySelectorAll('.nav__list-item a');
+const homeLink = document.querySelector('.home-link');
+const artistsLink = document.querySelector('.artists-link');
+const eventLink = document.querySelector('.events-link');
+const studioLink = document.querySelector('.studio-link');
 
 mobbDiv.addEventListener('mouseenter', () => {
 	mobbAudio.play();
@@ -25,4 +31,14 @@ const handleDate = () => {
 	currentYear.textContent = date;
 };
 
+const handleNavOpen = () => {
+	nav.classList.toggle('nav_open');
+	homeLink.classList.toggle('home_open');
+	artistsLink.classList.toggle('artists_open');
+	eventLink.classList.toggle('event_open');
+	studioLink.classList.toggle('studio_open');
+};
+
 handleDate();
+menuBtn.addEventListener('click', handleNavOpen);
+allLinks.forEach((link) => link.addEventListener('click', handleNavOpen));
